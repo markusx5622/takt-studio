174ad95f7e073e6acf34071c4e02085e6afeb50b
M&C Web Solutions <cooperdan598@gmail.com>
2026-05-01 11:57:45 +0200

    style: lighten background gray to slate-100 for tool sections

--- components/ConsultingBackground.tsx
+++ components/ConsultingBackground.tsx
@@ -2,6 +2,6 @@
 
 export default function ConsultingBackground() {
   return (
-    <div className="absolute inset-0 z-0 pointer-events-none bg-[#e2e8f0]" />
+    <div className="absolute inset-0 z-0 pointer-events-none bg-[#f1f5f9]" />
   )
 }
